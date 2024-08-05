f4120d330c744f067ecfd54cb18b9bcc3354990d
Darshil Ladani <ladani.darshil511@gmail.com>
2024-08-05 11:56:39 +0530

    creating a user defined file

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-import express from "express"
-import cors from "cors"
 import dotenv from "dotenv"
 dotenv.config()
+import express from "express"
+import cors from "cors"
+import bodyParser from 'body-parser'
+import fs from "fs"
+import mongoose from "mongoose"
+import multer from "multer"
+import GridFsStorage from "multer-gridfs-storage"
 
 const app = express();
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 app.get('/health', (req, res) => {
     res.status(200).send("Health status OK!")
@@ -14,5 +20,11 @@ app.get('/health', (req, res) => {
 app.get('/', (req, res) => {
     res.sendFile(`${process.env.PROJECT_PATH}` + "index.html")
 })
+
+app.post('/example', (req, res) => {
+    res.send(`File name is: ${req.body.fname}.`);
+    console.log(req.body.fname)
+    var writeStream = fs.createWriteStream("req.body.fname");
+});
 
 export default app
