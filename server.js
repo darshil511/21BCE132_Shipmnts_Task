80e05d9a6302ab582eb57c105cd93e09dc3dd87d
Darshil Ladani <ladani.darshil511@gmail.com>
2024-08-05 12:14:46 +0530

    files are created on server

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ import mongoose from "mongoose"
 import multer from "multer"
 import GridFsStorage from "multer-gridfs-storage"
 
+var files = []
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -24,7 +26,11 @@ app.get('/', (req, res) => {
 app.post('/example', (req, res) => {
     res.send(`File name is: ${req.body.fname}.`);
     console.log(req.body.fname)
-    var writeStream = fs.createWriteStream("req.body.fname");
+    var writeStream = fs.createWriteStream(`${req.body.fname}`);
+    writeStream.write(`${req.body.fcontents}`);
+    writeStream.end();
+    files.push(writeStream);
+    console.log(files.length);
 });
 
 export default app
